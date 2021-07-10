830f0e4ba38054121963018b2c3aec5b46824d4c
awesomeo184 <63030569+awesomeo184@users.noreply.github.com>
2021-07-10 20:32:08 +0900

    상품목록 컨트롤러, 뷰

---
+++ src/main/java/com/example/springmvc/web/basic/BasicItemController.java
@@ -0,0 +1,36 @@
+package com.example.springmvc.web.basic;
+
+import com.example.springmvc.domain.item.Item;
+import com.example.springmvc.domain.item.ItemRepository;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+* 여기서 itemRepository의 주입이 어떻게 일어나는가?
+* @RequiredArgsConstructor는 final이 붙은 필드의 생성자를 만들어줌 -> 생성자가 하나 있으면 @Autowired가 없어도 주입을 받을 수 있음
+* */
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
